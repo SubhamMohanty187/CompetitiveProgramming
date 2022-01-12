333b133c4e04cff6f59a96e1e26bffa30de0c690
SubhamMohanty187 <87409528+SubhamMohanty187@users.noreply.github.com>
2022-01-12 22:11:19 +0530

    Create CountingValleys.py
    Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.

---
+++ CountingValleys.py
@@ -0,0 +1,13 @@
+def countingValleys(steps, path):
+    # Write your code here
+    level = 0         # sea-level
+    valley = 0
+    for i in path:
+        if i == "U":
+            level += 1
+            if level == 0:   # Since we count a valley only after
+                valley += 1  # we come out of D
+        else:
+            level -= 1
+    
+    return valley
