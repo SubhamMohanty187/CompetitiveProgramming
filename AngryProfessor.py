bf2b3ede2961c16a480455acd5af3de604a95e5d
SubhamMohanty187 <87409528+SubhamMohanty187@users.noreply.github.com>
2022-01-13 21:27:28 +0530

    Create AngryProfessor.py
    A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, the professor decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time ( a[i] <= 0) to arrived late (a[i] > 0).
    
    Given the arrival time of each student and a threshold number of attendees, determine if the class is cancelled

---
+++ AngryProfessor.py
@@ -0,0 +1,13 @@
+def angryProfessor(k, a):
+    # Write your code here
+    n = 0
+    p = 0
+    for i in a:
+        if i <= 0:
+            p += 1
+        else:
+            n += 1
+    if p >= k:
+        return "NO"   # Classes will NOT be cancelled
+    else:
+        return "YES"  # Classes will be cancelled
