8d7d3a009eaaa5e7f01d585e0c08ba62d1bba916
Subham Mohanty <87409528+SubhamMohanty187@users.noreply.github.com>
2022-01-16 13:27:43 +0530

    Update A Very Big Sum.c

--- A Very Big Sum.c
+++ A Very Big Sum.c
@@ -31,7 +31,9 @@ int main()
     char* ar_count_str = readline();
     int ar_count = strtol(ar_count_str, &ar_count_endptr, 10);
 
-    if (ar_count_endptr == ar_count_str || *ar_count_endptr != '\0') { exit(EXIT_FAILURE); }
+    if (ar_count_endptr == ar_count_str || *ar_count_endptr != '\0') 
+    { exit(EXIT_FAILURE); }
+    
     char** ar_temp = split_string(readline());
     long* ar = malloc(ar_count * sizeof(long));
 
