659ab596f27b32905149a825f4adcbf4bf7d9eac
Subham Mohanty <87409528+SubhamMohanty187@users.noreply.github.com>
2022-01-28 12:38:01 +0530

    Create Jumping on the clouds.py

---
+++ Jumping on the clouds.py
@@ -0,0 +1,14 @@
+def jumpingOnClouds(c):
+    # Write your code here
+    i = 0
+    jump = 0
+    
+    while i < n-1:
+        if i + 2 >= n or c[i + 2] == 1:
+            i += 1
+            jump += 1
+        else:
+            i += 2
+            jump += 1
+            
+    return jump
