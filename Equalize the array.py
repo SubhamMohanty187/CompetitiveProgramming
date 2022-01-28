2480813acbe695e3fb900b1953b8433faae489d1
Subham Mohanty <87409528+SubhamMohanty187@users.noreply.github.com>
2022-01-28 13:01:51 +0530

    Update Equalize the array.py

--- Equalize the array.py
+++ Equalize the array.py
@@ -8,7 +8,7 @@ import sys
 from collections import Counter
 #
 # Complete the 'equalizeArray' function below.
-#
+
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
